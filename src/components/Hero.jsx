b87103d61473b112a0e29c60f09fa102ed591abf
k3nnynguy3n <114941124+k3nnynguy3n@users.noreply.github.com>
2025-06-26 14:22:35 -0700

    Change layout and color scheme to white/navy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,33 @@
 import './Hero.css';
+// import logo from '../assets/logo.png';
+// import profile from '../assets/profile.png';
 
 export default function Hero() {
   return (
     <section id="hero" className="hero">
-      <h1 className="animated-text">Hi, I'm Kenny Nguyen</h1>
+      <header className="hero-header">
+      {/*  <img src={logo} alt="Logo" className="logo" /> */}
+        <nav className="nav-links">
+          <a href="#hero">Home</a>
+          <a href="#about">About Me</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact Me</a>
+        </nav>
+      </header>
+
+
+      <div className="hero-content">
+        <div className="hero-text">
+          <h1>Hello! I'm glad you're here!</h1>
+          <h3>I'm Kenny Nguyen</h3>
+          <p>I’m an aspiring software engineer who loves uncovering the logic behind complex systems and transforming that understanding into real world solutions. </p>
+          <a href="#contact" className="contact-btn">Contact Me</a>
+        </div>
+        <div className="hero-image">
+          <div className="blob-bg"></div>
+        {/*  <img src={profile} alt="Kenny Nguyen" /> */}
+        </div>
+      </div>
     </section>
   );
 }
